ad6f5a44d2cb324766e3b310a8838e8e8101f95a
Joel Selvaraj <joel.devbox@protonmail.com>
2020-04-14 21:42:49 +0530

    Test
    Change-Id: I6ce1e74d65aa7fc36a7ba9174652c2a4cb23406b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,12 @@ AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
 # Camera
 TARGET_FACE_UNLOCK_CAMERA_ID := 5
 
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(DEVICE_PATH)/charger/images
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
@@ -36,6 +42,9 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+#allow arm64 libhybris
+HYBRIS_MEDIA_32_BIT_ONLY := false
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/beryllium/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Get non-open-source specific aspects
-#$(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2246
@@ -64,3 +64,59 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
+################
+# Ubuntu Touch #
+################
+
+# Wakelock
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sensors.wl_dis=true
+
+# droidmedia doesn't support HAL3 yet
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.camera.HAL3.enabled=0
+
+# Aethercast is currently disabled due to kernel panic, will get reenabled at some point.
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ubuntu.widi.supported=1
+
+PRODUCT_PACKAGES += \
+    libmedia_omx \
+    android.frameworks.displayservice@1.0 \
+    libion \
+    libmedia_compat_layer \
+    libsf_compat_layer \
+    libui_compat_layer \
+    libdroidmedia \
+    minimediaservice \
+    minisfservice \
+    miniafservice \
+    libbiometry_fp_api \
+    libubuntu_application_api \
+
+## Power off charging mode
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+## Disable FakeSensorServer so UBports can use it
+MINIMEDIA_SENSORSERVER_DISABLE := 1
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/halium/lib/udev/rules.d/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
+    $(LOCAL_PATH)/rootdir/system/halium/etc/ubuntu-touch-session.d/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
+    $(LOCAL_PATH)/rootdir/system/halium/etc/apparmor.d/abstractions/base:system/halium/etc/apparmor.d/abstractions/base \
+    $(LOCAL_PATH)/rootdir/system/halium/etc/apparmor.d/local/usr.bin.media-hub-server:system/halium/etc/apparmor.d/local/usr.bin.media-hub-server \
+    $(LOCAL_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android \
+    $(LOCAL_PATH)/rootdir/system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android 
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.disabled.rc:system/etc/init/init.disabled.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/etc/init/servicemanager.rc:system/etc/init/servicemanager.rc \
+    $(LOCAL_PATH)/rootdir/system/etc/init_hcismd_up.sh:system/etc/init_hcismd_up.sh \
+    $(LOCAL_PATH)/rootdir/system/halium/etc/init/ofono.override:system/halium/etc/init/ofono.override \
+    $(LOCAL_PATH)/rootdir/system/halium/etc/init/timekeeper.conf:system/halium/etc/init/timekeeper.conf \
+    $(LOCAL_PATH)/rootdir/system/halium/usr/share/upstart/sessions/usensord.conf:system/halium/usr/share/upstart/sessions/usensord.conf \
+    $(LOCAL_PATH)/rootdir/system/halium/usr/share/powerd/device_configs/config-default.xml:system/halium/usr/share/powerd/device_configs/config-default.xml
